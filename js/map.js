c2316d5532297bdf11e418c67e500ae1d6b7fe49
joaosousamartins <joao.sousa.martins@icloud.com>
2026-01-31 20:47:22 +0000

    Include long name in line name display

--- js/map.js
+++ js/map.js
@@ -102,7 +102,7 @@ function renderPatterns() {
   
   container.innerHTML = allPatterns.map(pattern => {
     const isSelected = pattern.shapeId === selectedPatternId;
-    const lineName = pattern.routeShortName ? `Linha ${pattern.routeShortName}` : 'Linha';
+    const lineName = pattern.routeShortName ? `Linha ${pattern.routeShortName} ${pattern.routeLongName || ''}` : 'Linha';
     const destination = pattern.headsign ||`${pattern.routeShortName || ''} - Sentido ${pattern.directionId === '0' ? 'Ida' : 'Volta'}``;
     
     return `
